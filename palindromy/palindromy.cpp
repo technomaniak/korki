9b426f4ea35cca728acff1ca04b8b3ab29057eda
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 13:55:01 +0100

    palyndromes

--- palindromy/palindromy.cpp
+++ palindromy/palindromy.cpp
@@ -4,61 +4,31 @@
 #include <fstream>
 using namespace std;
 
-bool countpalyndromes(string input) {
-
-    for(int i = (input.size()/2)-1; i>=0; i--) {
-        if (input[i] != input[input.size()-i-1]) {
+bool isPalindromic(const string &input)
+{
+    for (int i = 0; i < input.size() / 2; i++)
+    {
+        int k = input.size() - i - 1;
+        if (input[i] != input[k])
+        {
             return false;
         }
     }
-
     return true;
 }
 
-string dec2any(int input, int base_system) {
-    string output = "";
+int main()
+{
+    string input;
+    cout << "Provide an input string: ";
+    cin >> input;
 
-    while (input>0) {
-        int rest = input%base_system;
-
-        if (rest<10) {
-            output.insert(0, 1, '0'+rest);
-        }
-        else {
-            output.insert(0, 1, 'A'-10+rest);
-
-        }
-        input = input/base_system;
+    if (isPalindromic(input) == true)
+    {
+        cout << "Input is a palyndrome.";
     }
-    return output;
-}
-
-int main() {
-    int number, result, maxcount = 0;
-    string number2;
-
-    ifstream in("dane.txt");
-    ofstream out("wyniki4.txt");
-
-    while(!in.eof()) {
-        int count = 0;
-
-        in >> number;
-
-        for(int i = 2; i<=16; i++){
-            number2 = dec2any(number, i);
-
-            if(countpalyndromes(number2)==true) {
-                count++;
-            }
-        }
-        
-        if(count > maxcount) {
-            result = number;
-            maxcount = count;
-        }
+    else
+    {
+        cout << "Input is not a palyndrome.";
     }
-    cout << maxcount << "\n";
-    cout << result;
-    out << result;
 }
