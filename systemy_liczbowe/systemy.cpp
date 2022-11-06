2888661ee6b700e217e36561fbeb6649f1894f51
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 13:58:28 +0100

    fix

--- systemy_liczbowe/systemy.cpp
+++ systemy_liczbowe/systemy.cpp
@@ -2,46 +2,57 @@
 #include <string>
 using namespace std;
 
-int str2int(string input, int base_system) {
+int str2int(string input, int base_system)
+{
     int output = 0;
 
-    for (int i=0; i<input.size(); i++) {
-        if (input[i]<='9') {
-            output = output*base_system + input[i]-'0';
+    for (int i = 0; i < input.size(); i++)
+    {
+        if (input[i] <= '9')
+        {
+            output = output * base_system + input[i] - '0';
         }
-        else {
-            output = output*base_system + input[i]-'A'+10;
+        else
+        {
+            output = output * base_system + input[i] - 'A' + 10;
         }
     }
     return output;
 }
 
-string dec2any(int input, int base_system) {
+string dec2any(int input, int base_system)
+{
     string output = "";
 
-    while (input>0) {
-        int rest = input%base_system;
+    while (input > 0)
+    {
+        int rest = input % base_system;
 
-        if (rest<10) {
-            output.insert(0, 1, '0'+rest);
+        if (rest < 10)
+        {
+            output.insert(0, 1, '0' + rest);
         }
-        else {
-            output.insert(0, 1, 'A'-10+rest);
-
+        else
+        {
+            output.insert(0, 1, 'A' - 10 + rest);
         }
-        input = input/base_system;
+        input = input / base_system;
     }
     return output;
 }
 
-int main() {
+int main()
+{
     int input_base, output_base, i, test;
     string result = "";
     string input_number;
 
-    cout << "Input a number: "; cin >> input_number;
-    cout << "Input the numbers base: "; cin >> input_base;
-    cout << "Input requested base: "; cin >> output_base;
+    cout << "Input a number: ";
+    cin >> input_number;
+    cout << "Input the numbers base: ";
+    cin >> input_base;
+    cout << "Input requested base: ";
+    cin >> output_base;
 
     test = str2int(input_number, input_base);
     cout << dec2any(test, output_base) << "\n";
