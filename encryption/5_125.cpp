8e7fca68574e69bcec9fce71d87ab0f7db0f6264
BuildTools <unconfigured@null.spigotmc.org>
2022-11-27 17:50:10 +0100

    gay

--- encryption/5_125.cpp
+++ encryption/5_125.cpp
@@ -2,10 +2,48 @@
 #include <fstream>
 #include <string>
 
+// 123
+
 using namespace std;
 
 void encode(const string &input, const string &encryptor, string &output)
 {
+    int ignore = 0;
+    // iterating through input
+    for (int i = 0; i < input.size(); i++)
+    {
+        int move = (int)encryptor[(i - ignore) % encryptor.size()] - '0';
+
+        // checking if number is within range of encryption
+        if (input[i] >= 'A' && input[i] <= 'Z')
+        {
+            if (input[i] + move > 'Z')
+            {
+                int temp = 'Z' - (input[i]);
+                temp = move - temp - 1;
+                while (true)
+                {
+                    if ('A' + temp > 'Z')
+                    {
+                        temp = temp - 26;
+                    }
+                    else
+                    {
+                        output[i] = 'A' + temp;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                output[i] = input[i] + move;
+            }
+        }
+        else
+        {
+            ignore++;
+        }
+    }
 }
 
 int main()
